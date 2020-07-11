98c74a542a999417b201c0e18621b8ccfb149ed2
anurag-g <anuraggujarathi@gmail.com>
2020-07-11 16:35:15 +0530

    capture -> preview working

--- lib/pages/TalentVideoPreview.dart
+++ lib/pages/TalentVideoPreview.dart
@@ -24,7 +24,7 @@ class _TalentVideoPreviewState extends State<TalentVideoPreview> {
   @override
   void initState() {
     super.initState();
-    //OptionUtils.addDefaultOptions(controller);
+    OptionUtils.addDefaultOptions(controller);
     controller.setDataSource(
       DataSource.file(File(widget.videoPath)),
       autoPlay: true,
@@ -42,22 +42,99 @@ class _TalentVideoPreviewState extends State<TalentVideoPreview> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
+      body: Stack(
         children: <Widget>[
-          AspectRatio(
-            aspectRatio: 1280 / 720,
+          Container(
             child: IjkPlayer(
               mediaController: controller,
-              controllerWidgetBuilder: (ctl) {
-                return DefaultIJKControllerWidget(
-                  controller: ctl,
-                  volumeType: VolumeType.media,
-                );
-              },
             ),
           ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 90,
+              margin: EdgeInsets.only(bottom: 40),
+              color: Colors.black.withOpacity(0.5),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 50,
+                          width: 50,
+                          color: Colors.white,
+                        ),
+                        Container(
+                          height: 20,
+                          child: Text("Speed Up", style: TextStyle(color: Colors.white),),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 50,
+                          width: 50,
+                          color: Colors.blue,
+                        ),
+                        Container(
+                          height: 20,
+                          child: Text("Speed Up"),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          height: 50,
+                          width: 50,
+                          color: Colors.orange,
+                        ),
+                        Container(
+                          height: 20,
+                          child: Text("Speed Up"),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
+  }
+}
+
+class OptionUtils {
+  static void addDefaultOptions(IjkMediaController controller) {
+    controller.addIjkPlayerOptions(
+      [TargetPlatform.iOS, TargetPlatform.android],
+      createIJKOptions(),
+    );
+  }
+
+  static Set<IjkOption> createIJKOptions() {
+    return <IjkOption>[
+      IjkOption(IjkOptionCategory.player, "mediacodec", 1),
+      IjkOption(IjkOptionCategory.player, "mediacodec-hevc", 1),
+      IjkOption(IjkOptionCategory.player, "videotoolbox", 1),
+      IjkOption(IjkOptionCategory.player, "opensles", 0),
+      IjkOption(IjkOptionCategory.player, "overlay-format", 0x32335652),
+      IjkOption(IjkOptionCategory.player, "framedrop", 1),
+      IjkOption(IjkOptionCategory.player, "start-on-prepared", 0),
+      IjkOption(IjkOptionCategory.format, "http-detect-range-support", 0),
+      IjkOption(IjkOptionCategory.codec, "skip_loop_filter", 48),
+    ].toSet();
   }
 }
